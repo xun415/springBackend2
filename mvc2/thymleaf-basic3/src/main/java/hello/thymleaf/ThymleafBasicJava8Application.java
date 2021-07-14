9618d22853ad3bf8405ce1dbd7dec2388fb51db2
TH.Park <th.park@asolution.biz>
2021-07-14 13:37:32 +0900

    init commit

---
+++ mvc2/thymleaf-basic3/src/main/java/hello/thymleaf/ThymleafBasicJava8Application.java
@@ -0,0 +1,13 @@
+package hello.thymleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymleafBasicJava8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymleafBasicJava8Application.class, args);
+	}
+
+}
---
+++ mvc2/thymleaf-basic3/src/test/java/hello/thymleaf/ThymleafBasicJava8ApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.thymleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymleafBasicJava8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
